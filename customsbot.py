91757417bc554e8aff9c0d361da54b306eb1086f
Samwalton9 <swalton@wikimedia.org>
2018-01-08 20:15:04 +0000

    Version 1, three commands.

---
+++ customsbot.py
@@ -0,0 +1,268 @@
+import discord
+import asyncio
+import datetime
+
+#TODO: Fix timers so that they're countdowns to 5 minutes from the time the command is sent, not repeated second gaps.
+# Slows down when multiple commands are used otherwise.
+#TODO: Add a remove X message(s) command, since non-mods cant delete the bot's messages.
+#TODO: Randomly select in the case of tied emoji votes
+#TODO: Post password in mods, then SSSC, then publicly
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as', client.user.name)
+    print('\nLogged in to the following servers:')
+    for server in client.servers:
+        print(server.name)
+    print('-----')
+
+@client.event
+async def on_message(message):
+    is_command = message.content.startswith('$')
+
+    if is_command:
+        customs_channel = get_custom_games()
+        message_channel = message.channel
+        if message_channel == customs_channel['hosters']:
+            await parse_command(message)
+        else:
+            error_message = "Error: You must be in the #custom-hosters channel to do that."
+            await client.send_message(message_channel, content=error_message)
+
+def get_custom_games():
+    #customs_hosters = 382550498533703680  # bot-testing in my server
+    #customs_channel = client.get_channel(str(customs_hosters))  # Test in the same channel.
+
+    customs_hosters = client.get_channel(str(375276183777968130))  # Live
+    customs_channel = client.get_channel(str(317770788524523531))  # Live
+
+    channels = {'hosters': customs_hosters, 'games': customs_channel}
+
+    return channels
+
+async def parse_command(message_object):
+
+    command_string = message_object.content
+    command_channel = message_object.channel
+
+    split_command = command_string[1:].split(" ")
+    primary_command = split_command[0]
+    options = split_command[1:]
+
+    args = split_command[1:]
+    if primary_command in command_list:
+        result = await command_list[primary_command](message_object)
+    else:
+        result = "Error: That command doesn't exist. To see a list of available commands type $help."
+
+    if result:
+        await client.send_message(command_channel, content=result)
+
+async def squad_vote(command_message):
+    command_sent = command_message.content
+    message_squad_sizes = command_sent.split(" ")[1:]
+    
+    if len(message_squad_sizes) == 0:
+        default_squad_sizes = [1, 2, 4, 8]
+        squad_sizes_selected = default_squad_sizes
+    else:
+        if message_squad_sizes[0] == "all":
+            squad_sizes_selected = range(1,11)
+        else:
+            try:
+                sizes_selected = [int(i) for i in message_squad_sizes]
+                squad_sizes_selected = sorted(sizes_selected)
+            except ValueError:
+                return "Error: Please only use integers as arguments for `squadvote`."
+
+            squads_correct_range = all(i >= 1 and i <= 10 for i in squad_sizes_selected)
+            if not squads_correct_range:
+                return "Error: Please only use integers between 1 and 10 for `squadvote`."            
+
+    customs_channel = get_custom_games()
+
+    squad_vote_message = "Please vote on squad size for the next game:\nTimer: {}"
+    default_message = squad_vote_message.format("03:00")
+    
+    sent_squad_message = await client.send_message(customs_channel['games'], content= default_message)
+
+    for squad_size_int in squad_sizes_selected:
+        if squad_size_int == 10:
+            emoji = "\U0001F51F"  # :keycap_ten: is a single emoji
+        else:
+            emoji = str(squad_size_int) + "\U000020e3"
+
+        await client.add_reaction(sent_squad_message, emoji)
+
+    message_channel = command_message.channel
+    await client.send_message(message_channel, "Squad size vote successfully posted.")
+
+    countdown_timer = datetime.timedelta(seconds=180)
+    countdown_timer_string = get_countdown_string(countdown_timer)
+
+    while countdown_timer_string != "00:00":
+        countdown_timer = countdown_timer - datetime.timedelta(seconds=1)
+        countdown_timer_string = get_countdown_string(countdown_timer)
+        await asyncio.sleep(1)
+        new_message = squad_vote_message.format(countdown_timer_string)
+        await client.edit_message(sent_squad_message, new_message)
+
+    sent_squad_message = await client.get_message(customs_channel['games'], sent_squad_message.id)
+    squad_selected = most_reactions(sent_squad_message)
+
+    squad_result = squad_selected + "P Squads"
+
+    await client.clear_reactions(sent_squad_message)
+    squad_message_finished = "Squad size vote over. Result: {}".format(squad_result)
+    await client.edit_message(sent_squad_message, squad_message_finished)
+
+    await set_voice_limit(user_limit=squad_selected)
+
+def most_reactions(message):
+    message_reactions = message.reactions
+    reaction_emojis, reaction_count = [], []
+    for message_reaction in message_reactions:
+        reaction_emojis.append(message_reaction.emoji)
+        reaction_count.append(message_reaction.count)
+
+    max_emoji = [reaction_emojis[i] for i,x in enumerate(reaction_count) if x == max(reaction_count)]
+
+    return max_emoji[0]
+
+async def region_vote(command_message):
+    regions = [
+               333366981959090186,  # Asia
+               333366950455672833,  # EU
+               379707501282590720,  # KJP
+               333366997729411082,  # NA
+               333366933657223168,  # OCE
+               333366961792876544,  # SA
+               333366971586445313,  # SEA
+               ]
+
+    customs_channel = get_custom_games()
+
+    region_vote_message = "Which region should we host today's games on?\nTimer: {}"
+    default_region_message = region_vote_message.format("03:00")
+    sent_region_message = await client.send_message(customs_channel['games'], content= default_region_message)
+
+    for region_emoji in regions:
+        region_emoji_obj = discord.utils.get(client.get_all_emojis(), id=str(region_emoji))
+        await client.add_reaction(sent_region_message, region_emoji_obj)
+
+    countdown_timer = datetime.timedelta(seconds=180)
+    countdown_timer_string = get_countdown_string(countdown_timer)
+
+    while countdown_timer_string != "00:00":
+        countdown_timer = countdown_timer - datetime.timedelta(seconds=1)
+        countdown_timer_string = get_countdown_string(countdown_timer)
+        await asyncio.sleep(1)
+        new_message = region_vote_message.format(countdown_timer_string)
+        await client.edit_message(sent_region_message, new_message)
+
+    sent_region_message = await client.get_message(customs_channel['games'], sent_region_message.id)
+    region_selected = most_reactions(sent_region_message)
+
+    region_result = region_selected
+
+    await client.clear_reactions(sent_region_message)
+    region_message_finished = "Region vote over. Result: {}".format(region_result)
+    await client.edit_message(sent_region_message, region_message_finished)
+
+def get_countdown_string(timedelta_object):
+    countdown_timer_split = str(timedelta_object).split(":")
+    countdown_timer_string = ":".join(countdown_timer_split[1:])
+    return countdown_timer_string
+
+async def password_countdown(command_message):
+    split_message = command_message.content.split(" ")
+    message_length = len(split_message)
+    if message_length not in [2, 3]:
+        return "Incorrect number of arguments given."
+
+    password = split_message[1]
+    if message_length == 3:
+        timer = split_message[2]
+        try:
+            num_seconds = int(split_message[2])*60
+        except ValueError:
+            return "Error: Please use an integer to denote the countdown length for `password`."
+    else:
+        num_seconds = 300
+    customs_channel = get_custom_games()
+
+    countdown_timer = datetime.timedelta(seconds=num_seconds)
+    countdown_timer_string = get_countdown_string(countdown_timer)
+
+    template_string = "Server name: PUBG Reddit\nPassword: {}"
+
+    default_text = template_string.format("[" + countdown_timer_string + "]")
+
+    countdown_message = await client.send_message(customs_channel['games'], default_text)
+
+    while countdown_timer_string != "00:00":
+        countdown_timer = countdown_timer - datetime.timedelta(seconds=1)
+        countdown_timer_string = get_countdown_string(countdown_timer)
+        await asyncio.sleep(1)
+        new_message = template_string.format("[" + countdown_timer_string + "]")
+        await client.edit_message(countdown_message, new_message)
+
+    result_string = template_string.format(password)
+    await client.edit_message(countdown_message, result_string)
+
+async def set_voice_limit(command_message=None, user_limit=None):
+    if command_message:
+        split_message = command_message.content.split(" ")
+        message_length = len(split_message)
+        if message_length == 2:
+            voice_limit = split_message[1]
+            try:
+                voice_limit_int = int(voice_limit)
+            except ValueError:
+                return "Error: Please use an integer to denote the size of voice channels."    
+        else:
+            return "Error: Wrong number of arguments given."
+    if user_limit:
+        if len(user_limit) > 1:
+            voice_limit_int = int(user_limit[:-1])  # Unicode to int
+        else:
+            voice_limit_int = 10
+
+    customs_channel = get_custom_games()
+
+    all_channels = client.get_all_channels()
+    for channel in all_channels:
+        if channel.name.startswith("\U0001F6E0"):
+            await client.edit_channel(channel, user_limit=voice_limit_int)
+
+    voice_limit_message = "Set custom games voice channels to new user limit of {}.".format(voice_limit_int)
+
+    await client.send_message(customs_channel['hosters'], voice_limit_message)
+
+async def command_list(command_message):
+    command_channel = command_message.channel
+    list_of_commands = '''
+    `squadvote <squad size 1> <squad size 2> ...` - Post a vote for squad size for the next game, defaults to 1, 2, 4, and 8. `squadvote all` will do every size between 1 and 10.
+    `regionvote` - Post a vote for the region for this session's custom games.
+    `password <password> <minutes>` - Post a countdown to the password release of <minutes> minutes. 
+    '''
+    em = discord.Embed(title="CustomsBot commands", description= list_of_commands)
+
+    await client.send_message(command_channel, embed=em)
+
+command_list = {
+    'help': command_list,
+    'squadvote': squad_vote,
+    'sqv': squad_vote,
+    'regionvote': region_vote,
+    'rv': region_vote,
+    'password': password_countdown,
+    'setvoicelimit': set_voice_limit
+}
+
+with open('bot_token') as f:
+    token = f.readline().strip()
+
+client.run(token)
