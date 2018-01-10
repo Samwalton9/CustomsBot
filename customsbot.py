d1288e99c4d3eb5daa42dd0c3c850a436c4724a0
Samwalton9 <swalton@wikimedia.org>
2018-01-10 00:27:31 +0000

    Added comments and docstrings

--- customsbot.py
+++ customsbot.py
@@ -15,6 +15,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    """Check every message for a command, trust any from #custom-hosters"""
     is_command = message.content.startswith('$')
 
     if is_command:
@@ -27,8 +28,9 @@ async def on_message(message):
             await client.send_message(message_channel, content=error_message)
 
 def get_custom_games():
-    customs_hosters = client.get_channel(str(375276183777968130))  # Live
-    customs_channel = client.get_channel(str(317770788524523531))  # Live
+    """Returns a dict containing #custom-games and #custom-hosters objects"""
+    customs_hosters = client.get_channel(str(375276183777968130))
+    customs_channel = client.get_channel(str(317770788524523531))
 
     if debug:
         customs_hosters = client.get_channel(str(382550498533703680))  # bot-testing in my server
@@ -39,10 +41,14 @@ def get_custom_games():
     return channels
 
 def log_command(message_object, text):
+    """Whenever a command is sent, log it to the terminal"""
     print(message_object.timestamp, "| COMMAND |", text, "|", message_object.author.name)
 
 async def parse_command(message_object):
-
+    """
+    After a command has been identified, get the primary command and run
+    the relevant function.
+    """
     command_string = message_object.content
     command_channel = message_object.channel
 
@@ -53,13 +59,23 @@ async def parse_command(message_object):
     if primary_command in command_list:
         result = await command_list[primary_command](message_object)
     else:
-        print("INCORRECT COMMAND |", primary_command, "|", message_object.author.name)
+        print(message_object.timestamp, "| INCORRECT COMMAND |", primary_command, "|", message_object.author.name)
         result = "Error: That command doesn't exist. To see a list of available commands type $help."
 
     if result:
         await client.send_message(command_channel, content=result)
 
 async def squad_vote(command_message):
+    """
+    Starts a vote on squad size.
+
+    The hoster can specify squad sizes to be used, or 'all' to include
+    every size between 1 and 10. If there are no arguments, the vote
+    defaults to 1, 2, 4, and 8. The vote stays open for 3 minutes.
+
+    Once a winner is determined, set_voice_limit() is run for the
+    winning size.
+    """
     command_sent = command_message.content
     message_squad_sizes = command_sent.split(" ")[1:]
     
@@ -123,6 +139,10 @@ async def squad_vote(command_message):
     await set_voice_limit(user_limit=squad_selected)
 
 def most_reactions(message):
+    """
+    Given a message, determine which reaction emoji received the
+    most votes. Breaks ties with a random choice.
+    """
     message_reactions = message.reactions
     reaction_emojis, reaction_count = [], []
     for message_reaction in message_reactions:
@@ -139,7 +159,13 @@ def most_reactions(message):
         return max_emoji[i]
 
 async def region_vote(command_message):
-    regions = [
+    """
+    Starts a vote on region to host on.
+
+    Hoster has no control over this one; it just adds every region's emoji
+    and determines the winner.
+    """
+    region_emoji_ids = [
                333366981959090186,  # Asia
                333366950455672833,  # EU
                379707501282590720,  # KJP
@@ -155,7 +181,7 @@ async def region_vote(command_message):
     default_region_message = region_vote_message.format("03:00")
     sent_region_message = await client.send_message(customs_channel['games'], content= default_region_message)
 
-    for region_emoji in regions:
+    for region_emoji in region_emoji_ids:
         region_emoji_obj = discord.utils.get(client.get_all_emojis(), id=str(region_emoji))
         await client.add_reaction(sent_region_message, region_emoji_obj)
 
@@ -184,11 +210,20 @@ async def region_vote(command_message):
     await client.edit_message(sent_region_message, region_message_finished)
 
 def get_countdown_string(timedelta_object):
+    """Given a timedelta object, returns a mm:ss time string"""
     countdown_timer_split = str(timedelta_object).split(":")
     countdown_timer_string = ":".join(countdown_timer_split[1:])
     return countdown_timer_string.split(".")[0]  # Without decimal
 
 async def password_countdown(command_message):
+    """
+    Starts a countdown to password release in #custom-games. Immediately
+    posts server name and password to #mods and #super-secret-sub-club,
+    pinging @here in SSSC only.
+
+    Hoster must specify a password, and may optionally specify the number
+    of minutes until the password is released. Defaults to 5 minutes.
+    """
     split_message = command_message.content.split(" ")
     message_length = len(split_message)
     if message_length not in [2, 3]:
@@ -238,6 +273,12 @@ async def password_countdown(command_message):
     await client.send_message(customs_channel['games'], content="@here")
 
 async def set_voice_limit(command_message=None, user_limit=None):
+    """
+    Changes all custom games voice channel sizes to the one specified.
+
+    Can be run either directly via $setvoicelimit or indirectly in
+    other functions.
+    """
     if command_message:
         split_message = command_message.content.split(" ")
         message_length = len(split_message)
@@ -267,6 +308,13 @@ async def set_voice_limit(command_message=None, user_limit=None):
     await client.send_message(customs_channel['hosters'], voice_limit_message)
 
 async def remove_messages(command_message):
+    """
+    Removes a number of CustomsBot messages from #custom-games.
+
+    Hosters aren't necessarily mods, so they need some way to clear
+    CustomsBot's messages. This function removes either a set amount
+    or all of CustomsBot's messages.
+    """
     message_split = command_message.content.split(" ")
     message_length = len(message_split)
     num_messages = message_split[1]
@@ -283,6 +331,9 @@ async def remove_messages(command_message):
 
     message_count = 0
     messages_to_remove = []
+
+    # Defaults to only retrieving 100 messages from #custom-games.
+    # Has the potential to cause an issue, but should rarely ever do so.
     async for message in client.logs_from(customs_channel['games']):
         if message.author.id == str(399360578956689409):
             if num_messages == 'all':
@@ -302,6 +353,7 @@ async def remove_messages(command_message):
     log_command(command_message, "Remove messages ({})".format(num_messages))
 
 async def help_list(command_message):
+    """Prints an embedded list of commands."""
     command_channel = command_message.channel
     list_of_commands = '''
     `squadvote <squad size 1> <squad size 2> ...` - Post a vote for squad size for the next game, defaults to 1, 2, 4, and 8. `squadvote all` will do every size between 1 and 10. Automatically changes voice channel sizes to winning vote.
@@ -314,6 +366,7 @@ async def help_list(command_message):
 
     await client.send_message(command_channel, embed=em)
 
+# Keys are the commands used by helpers, values are the function names.
 command_list = {
     'help': help_list,
     'squadvote': squad_vote,
@@ -325,6 +378,9 @@ command_list = {
     'clear': remove_messages
 }
 
+# Debugging suppresses #mods and #super-secret-sub-club messages and
+# treats #bot-testing in SamWalton's Discord server as both #custom-games
+# and #custom-hosters.
 debug = True
 
 with open('bot_token') as f:
