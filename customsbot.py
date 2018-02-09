3755c4e4554e5f61709e7f4d2f427b378a494d5f
Samwalton9 <swalton@wikimedia.org>
2018-02-09 13:02:09 +0000

    Change regionvote debugging behaviour to actually add emojis

--- customsbot.py
+++ customsbot.py
@@ -270,15 +270,22 @@ async def region_vote(ctx):
     Hoster has no control over this one; it just adds every region's emoji
     and determines the winner.
     """
-    region_emoji_ids = [
-               '333366981959090186',  # Asia
-               '333366950455672833',  # EU
-               '379707501282590720',  # KJP
-               '333366997729411082',  # NA
-               '333366933657223168',  # OCE
-               '333366961792876544',  # SA
-               '333366971586445313',  # SEA
-               ]
+    if debug:
+        region_emoji_ids = [
+                   '409001479555514378', # test1
+                   '409001567069405194' # test2
+                   ]
+    else:
+        region_emoji_ids = [
+                   '333366981959090186',  # Asia
+                   '333366950455672833',  # EU
+                   '379707501282590720',  # KJP
+                   '333366997729411082',  # NA
+                   '333366933657223168',  # OCE
+                   '333366961792876544',  # SA
+                   '333366971586445313',  # SEA
+                   ]
+
 
     customs_channel = get_custom_games()
 
@@ -288,14 +295,10 @@ async def region_vote(ctx):
     region_message = await client.send_message(customs_channel,
                                                content= default_region_message)
 
-    if debug:
-        debug_text = "[Regions added]"
-        await client.send_message(customs_channel, debug_text)
-    else:
-        for region_emoji in region_emoji_ids:
-            region_emoji_obj = discord.utils.get(client.get_all_emojis(),
-                                                 id=region_emoji)
-            await client.add_reaction(region_message, region_emoji_obj)
+    for region_emoji in region_emoji_ids:
+        region_emoji_obj = discord.utils.get(client.get_all_emojis(),
+                                             id=region_emoji)
+        await client.add_reaction(region_message, region_emoji_obj)
 
     here_ping = await client.send_message(customs_channel,
                                           content="@here")
@@ -501,7 +504,7 @@ async def on_command_error(error, ctx):
 # Debugging suppresses #mods and #super-secret-sub-club messages and
 # treats #bot-testing in SamWalton's Discord server as both #custom-games
 # and #custom-hosters.
-debug = False
+debug = True
 
 if debug == True:
     token_file = 'test_bot_token'
